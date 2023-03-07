1e379271baa0279aa9848e6e500822c2d065c0cb
aleesan7 <ka_sanchezsamayoa@outlook.com>
2023-03-06 22:15:14 -0600

    API que consume AWS rekognition para el analisis de imagenes con el fin de obtener labels que describan la imagen

---
+++ app.py
@@ -0,0 +1,40 @@
+from cProfile import label
+import boto3
+import base64
+from io import BytesIO
+from flask import Flask, request, jsonify
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+AWS_REGION_NAME = os.getenv('AWS_REGION_NAME')
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+
+app = Flask(__name__)
+
+@app.route('/hoja1-201114336', methods=['POST'])
+def recognize_text():
+
+    # Obtener la imagen en base64 desde la solicitud POST
+    img_base64 = request.json['image']
+    # Decodificar la imagen en base64 a bytes
+    img_data = base64.b64decode(img_base64)
+
+    # Instanciar el objeto client para utilizar rekognition
+    client = boto3.client('rekognition',region_name=AWS_REGION_NAME,
+                           aws_access_key_id=AWS_ACCESS_KEY_ID,
+                           aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+
+    # Obtenemos respuesta de AWS
+    response = client.detect_labels(Image={'Bytes': img_data})
+
+    # Obtenemos labels
+    label_detections = response['Labels']
+
+    # Retornamos respuesta
+    return label_detections
+
+if __name__ == '__main__':
+    app.run()
